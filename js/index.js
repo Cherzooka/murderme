33d45d8f82d4cba8919a24f4304be03c3672a87e
Cherzooka <lady.cherette@gmail.com>
2016-11-17 16:28:06 -0600

    mmhdmg
    fghd

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $(".tabs-menu a").click(function(event) {
+    event.preventDefault();
+    $(this).parent().addClass("current");
+    $(this).parent().siblings().removeClass("current");
+    var tab = $(this).attr("href");
+    $(".tab-content").not(tab).css("display", "none");
+    $(tab).fadeIn();
+  });
+});
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("right-bar-sorting-container").style.height = "250px";
+    document.getElementById("right-bar-characters").style.marginTop = "250px";
+	$(".slide-button-open").fadeToggle("slow");
+	$(".slide-button-close").fadeToggle("slow");
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("right-bar-sorting-container").style.height = "0";
+    document.getElementById("right-bar-characters").style.marginTop = "0";
+	$(".slide-button-open").fadeToggle("slow");
+	$(".slide-button-close").fadeToggle("slow");
+}
+
+$(document).ready(function() {
+ $('.character-select').load('character-select.html');
+});
